5fedf92ad37bf555929650d12a38585854724254
camilosoto52 <juancamilosoto52@gmail.com>
2024-09-14 22:29:00 -0500

    Product Class

--- Product.java
+++ Product.java
@@ -1,34 +1,37 @@
 public class Product {
-    public enum Category_Smartphones {
+    public enum Smartphones {
         Iphone14, Iphone15, Iphone16, SAmsungA10, SamsungA30 , Huaweip30, HuaweiY9, RedmiNote10
     }
-    public enum Category_Accessories{
+    public enum Accessories{
         Flash_drive, Sd_card, Portable_Battery, Bluetooth_Speaker, Phone_Holder, Headphones, Phone_Case
     }
 
     private String name;
-    private Category_Smartphones category1;
-    private Category_Accessories category2;
+    private Smartphones category1;
+    private Accessories category2;
     
-    public Product(String name, Category_Smartphones category1, Category_Accessories category2 ) {
+    public Product(String name, Smartphones category1) {
         this.name = name;
         this.category1 = category1;
+    }
+    public Product(String name, Accessories category2) {
+        this.name = name;
         this.category2 = category2;
     }
 
-    public String getNombre() {
+    public String getName() {
         return name;
     }
-    public Category_Smartphones getCategory_Smartphones() {
+    public Smartphones getSmartphones() {
         return category1;
     }
-    public Category_Accessories getCategory_Accessories() {
+    public Accessories getCategory_Accessories() {
         return category2;
     }
     
     @Override
     public String toString() {
-        return "Producto: " + name + " | Category_Smartphones: " + category1 + " | Category_Accessories " + category2;
+        return "Product: " + name + " | Smartphones: " + category1 + " | Accessories " + category2;
     }
 
 }
